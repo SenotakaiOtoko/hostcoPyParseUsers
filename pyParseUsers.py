0b9ed9ce9daf57322ccbdb85699b1e675e5ff0e2
Viktor Rybakov <V.Rybakov@hostco.ru>
2018-04-09 09:53:19 +0500

    Added base functionality

--- pyParseUsers.py
+++ pyParseUsers.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class user:
+	def __init__(self, avatar, name, info):
+		self.avatar = avatar
+		self.info = info
+		self.name = name
+	avatar = ""
+	info = ""
+	name = ""
+
+class HttpProcessor(BaseHTTPRequestHandler):
+	table = ""
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('content-type','text/html')
+		self.end_headers()
+		self.wfile.write(table.encode("utf-8"))
+	
 driver = webdriver.chrome.webdriver.WebDriver('chromedriver.exe')
 
 #driver = webdriver.Firefox()
@@ -10,19 +29,37 @@ assert "ХОСТ" in driver.title
 
 
 elems = driver.find_elements_by_css_selector("#content > table > tbody > tr > td > p > a")
-users = []
+user_links = []
 for el in elems:
-	users.append(el.get_attribute("href"))
+	user_links.append(el.get_attribute("href"))
 
-for usr in users:
-	driver.get(usr)
-	#avatar = driver.find_element_by_class("user-avatar-personal")
-	#info = driver.find_element_by_class("user-info-personal")
+user_list = []
+
+for lnk in user_links:
+	driver.get(lnk)
+	avatar = driver.find_element_by_css_selector(".user-avatar-personal img")
+	info = driver.find_element_by_class_name("user-info-personal")
+	name = driver.find_element_by_class_name("page-title")
+	user_list.append(user(avatar.get_attribute("src"), name, info.text))
+
+table = "<table><tboby>"
+for usr in user_list:
+	table = table + "<tr><td><img src=\"" + usr.avatar + "\"><\td>"
+	table = table + "<td>" + usr.name + "</td>"
+	table = table + "<td>" + usr.info + "</td></tr>"
+table = table + "</tbody></table>"
+
+print(table)
+
+driver.close()
+
+#serv = HTTPServer(("localhost",80),HttpProcessor.init(table))
+#serv.serve_forever()
+
 
 	
 #elem.send_keys("pycon")
 #elem.send_keys(Keys.RETURN)
 #assert "No results found." not in driver.page_source
-driver.close()
 
 #content > table > tbody > tr:nth-child(2) > td:nth-child(2) > a
